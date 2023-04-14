51438faf23c65c450841faffc09c9f0f46f0f0fb
703145 <61280484+703145@users.noreply.github.com>
2023-04-15 00:57:16 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,56 +1,70 @@
-const weapons = [
-  { name: 'Short Sword', rarity: 'common' },
-  { name: 'Long Sword', rarity: 'uncommon' },
-  { name: 'Great Sword', rarity: 'rare' },
-  { name: 'Dagger', rarity: 'common' },
-  { name: 'Bow', rarity: 'uncommon' },
-  { name: 'Crossbow', rarity: 'rare' }
-];
 
-const armors = [
-  { name: 'Leather Armor', rarity: 'common' },
-  { name: 'Chainmail Armor', rarity: 'uncommon' },
-  { name: 'Plate Armor', rarity: 'rare' },
-  { name: 'Cloth Robes', rarity: 'common' },
-  { name: 'Studded Leather Armor', rarity: 'uncommon' },
-  { name: 'Scale Armor', rarity: 'rare' }
-];
-
-function getRandomItems(list, rarity, numItems) {
-  const filteredList = list.filter(item => item.rarity === rarity);
-  const availableItems = [];
-  for (let i = 0; i < numItems; i++) {
-    const randomIndex = Math.floor(Math.random() * filteredList.length);
-    const item = filteredList[randomIndex];
-    availableItems.push(item);
+  function generateShop() {
+    var weapons = [
+      {name: "Short Sword", rarity: "common"},
+      {name: "Long Sword", rarity: "uncommon"},
+      {name: "Great Sword", rarity: "rare"}
+    ];
+    
+    var armours = [
+      {name: "Leather Armour", rarity: "common"},
+      {name: "Chainmail Armour", rarity: "uncommon"},
+      {name: "Plate Armour", rarity: "rare"}
+    ];
+    
+    var shop = [];
+    var size = document.getElementById("shopSize").value;
+    var commonChance = 90;
+    var uncommonChance = 50;
+    var rareChance = 10;
+    
+    if (size === "normal") {
+      commonChance = 50;
+      uncommonChance = 30;
+      rareChance = 5;
+    } else if (size === "well-stocked") {
+      commonChance = 20;
+      uncommonChance = 10;
+      rareChance = 2;
+    }
+    
+    for (var i = 0; i < 10; i++) {
+      var weaponRarity = pickRarity(commonChance, uncommonChance, rareChance);
+      var armourRarity = pickRarity(commonChance, uncommonChance, rareChance);
+      var weapon = pickItemByRarity(weapons, weaponRarity);
+      var armour = pickItemByRarity(armours, armourRarity);
+      shop.push(weapon);
+      shop.push(armour);
+    }
+    
+    displayShop(shop);
   }
-  return availableItems;
-}
-
-function generateItems() {
-  const itemList = document.getElementById('item-list');
-  itemList.innerHTML = '';
   
-  const shopType = document.getElementById('shop-type').value;
-  const commonChance = (shopType === 'well-stocked') ? 0.9 : ((shopType === 'normal') ? 0.7 : 0.5);
-  
-  const weaponsList = getRandomItems(weapons, 'common', Math.floor(Math.random() * 6) + 5)
-                     .concat(getRandomItems(weapons, 'uncommon', Math.floor(Math.random() * 3) + 1))
-                     .concat(getRandomItems(weapons, 'rare', Math.floor(Math.random() * 2)));
-                     
-  const armorsList = getRandomItems(armors, 'common', Math.floor(Math.random() * 3) + 2)
-                    .concat(getRandomItems(armors, 'uncommon', Math.floor(Math.random() * 2)))
-                    .concat(getRandomItems(armors, 'rare', Math.floor(Math.random() * 1)));
-  
-  const availableItems = weaponsList.concat(armorsList);
-  
-  for (let item of availableItems) {
-    const rarityChance = (item.rarity === 'common') ? commonChance : ((item.rarity === 'uncommon') ? 0.3 : 0.1);
-    const randomChance = Math.random();
-    if (randomChance <= rarityChance) {
-      const itemElement = document.createElement('li');
-      itemElement.textContent = item.name;
-      itemList.appendChild(itemElement);
+  function pickRarity(commonChance, uncommonChance, rareChance) {
+    var rarity = Math.floor(Math.random() * 100);
+    if (rarity < commonChance) {
+      return "common";
+    } else if (rarity < uncommonChance) {
+      return "uncommon";
+    } else {
+      return "rare";
     }
   }
-}
+  
+  function pickItemByRarity(items, rarity) {
+    var itemsByRarity = items.filter(function(item) {
+      return item.rarity === rarity;
+    });
+    var randomIndex = Math.floor(Math.random() * itemsByRarity.length);
+    return itemsByRarity[randomIndex];
+  }
+  
+  function displayShop(shop) {
+    var shopList = document.getElementById("shopList");
+    shopList.innerHTML = "";
+    for (var i = 0; i < shop.length; i++) {
+      var listItem = document.createElement("li");
+      listItem.innerText = shop[i].name;
+      shopList.appendChild(listItem);
+    }
+  }
